2f13ebcdf5ceb2cf2e7c1b100160004b89823a90
Matisse Mallette <matissemallette@gmail.com>
2022-12-06 15:43:07 -0700

    create remote repo

--- spec/features/apartments/index_spec.rb
+++ spec/features/apartments/index_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Apartment index' do 
   before :each do 
-    @apartment = Apartment.create(name: "Sierra Foothills", rent: 100, rent_controlled: false, address: "48301 W 44th St Phoenix Arizona")
+    @apartment1 = Apartment.create(name: "Sierra Foothills", rent: 100, rent_controlled: false, address: "48301 W 44th St Phoenix Arizona")
+    @apartment2 = Apartment.create(name: "Blah Blah", rent: 20, rent_controlled: true, address: "123 Cityberg Statesville")
   end
 
   it 'displays a list of all apartments' do 
     visit '/apartments'
-    binding.pry
+    # save_and_open_page
     expect(page).to have_content("Sierra Foothills")
+    expect(page).to have_content("Blah Blah")
   end
 end
