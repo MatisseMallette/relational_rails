8e371acc916426e82f3904d8a93d531982282797
Matisse Mallette <matissemallette@gmail.com>
2022-12-07 06:08:26 -0700

    complete user_story_6

--- spec/features/apartments/show_spec.rb
+++ spec/features/apartments/show_spec.rb
@@ -8,9 +8,21 @@ RSpec.describe 'Apartment show' do
 
   it 'displays the information of an apartment with a certain id' do 
     visit "/apartments/#{@apartment1.id}"
-    save_and_open_page
+    # save_and_open_page
     expect(page).to have_content("Sierra Foothills")
     expect(page).to have_content("100")  
     expect(page).to have_content("48301 W 44th St Phoenix Arizona")
+  end
+
+  it 'displays a count of the number of tenants associated with each apartment' do 
+    tenant1 = @apartment1.tenants.create!(name: "Bob", number_of_pets: 1, has_job: false)  
+    tenant2 = @apartment1.tenants.create!(name: "Jim", number_of_pets: 100, has_job: false)
+    tenant3 = @apartment2.tenants.create!(name: "Daniel", number_of_pets: 0, has_job: false)
+    visit "/apartments/#{@apartment1.id}"
+    # save_and_open_page
+    expect(page).to have_content("Number of tenants: 2")
+    visit "/apartments/#{@apartment2.id}"
+    # save_and_open_page
+    expect(page).to have_content("Number of tenants: 1")
   end
 end
